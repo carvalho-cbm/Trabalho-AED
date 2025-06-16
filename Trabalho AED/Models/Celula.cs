96be08cfe73efb129da4dd47e9ef50cff91c1632
carvalho_cbm <arthurcbm2002@gmail.com>
2025-06-16 04:10:59 -0300

    Adicionado conteúdo de fila flexível

---
+++ Trabalho AED/Models/Celula.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_AED.Models
+{
+    internal class Celula
+    {
+        private Candidato elemento;
+        private Celula prox;
+        public Celula(Candidato elemento)
+        {
+            this.elemento = elemento;
+            this.prox = null;
+        }
+        public Celula()
+        {
+            this.elemento = null;
+            this.prox = null;
+        }
+        public Celula Prox
+        {
+            get { return prox; }
+            set { prox = value; }
+        }
+        public Candidato Elemento
+        {
+            get { return elemento; }
+            set { elemento = value; }
+        }
+    }
+}
--- Trabalho AED/Models/FilaFlexivel.cs
+++ Trabalho AED/Models/FilaFlexivel.cs
@@ -8,5 +8,37 @@ namespace Trabalho_AED.Models
 {
     internal class FilaFlexivel
     {
+        private Celula primeiro, ultimo;
+        public FilaFlexivel()
+        {
+            primeiro = new Celula();
+            ultimo = primeiro;
+        }
+
+        public void Inserir(Candidato x)
+        {
+            ultimo.Prox = new Celula(x);
+            ultimo = ultimo.Prox;
+        }
+
+        public string Remover()
+        {
+            if (primeiro == ultimo)
+                throw new Exception("Erro!");
+            Celula tmp = primeiro;
+            primeiro = primeiro.Prox;
+            Candidato elemento = primeiro.Elemento;
+            tmp.Prox = null;
+            tmp = null;
+            return elemento.NomeCandidato;
+        }
+
+        public void Mostrar()
+        {
+            for (Celula i = primeiro.Prox; i != null; i = i.Prox)
+            {
+                Console.WriteLine(i.Elemento);
+            }
+        }
     }
 }
