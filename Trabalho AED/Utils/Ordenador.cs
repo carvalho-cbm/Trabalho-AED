d78d736152b011a59e518db07d31a50b2ba1748e
Gabriel <gabriprs3010@gmail.com>
2025-06-17 10:46:11 -0300

    Ordenador.cs e ProcessadorDeCandidatos.cs feitos

--- Trabalho AED/Utils/Ordenador.cs
+++ Trabalho AED/Utils/Ordenador.cs
@@ -4,9 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Trabalho_AED.Utils
+public class Ordenador
 {
-    internal class Ordenador
+    // Método público para ordenar a lista de candidatos
+    public static void QuickSort(List<Candidato> lista)
     {
+        QuickSort(lista, 0, lista.Count - 1);
+    }
+
+    // Implementação recursiva do QuickSort
+    private static void QuickSort(List<Candidato> lista, int esq, int dir)
+    {
+        int i = esq, j = dir;
+        Candidato pivo = lista[(esq + dir) / 2];
+
+        while (i <= j)
+        {
+            while (Comparar(lista[i], pivo) > 0) i++;
+            while (Comparar(lista[j], pivo) < 0) j--;
+
+            if (i <= j)
+            {
+                var temp = lista[i];
+                lista[i] = lista[j];
+                lista[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (esq < j) QuickSort(lista, esq, j);
+        if (i < dir) QuickSort(lista, i, dir);
+    }
+
+    // Retorna >0 se a1 deve vir antes de a2 (ordem decrescente)
+    private static int Comparar(Candidato a1, Candidato a2)
+    {
+        if (a1.NotaMedia != a2.NotaMedia)
+            return -a1.NotaMedia.CompareTo(a2.NotaMedia); // decrescente
+        else
+            return -a1.NotaRedacao.CompareTo(a2.NotaRedacao); // decrescente
     }
 }
--- Trabalho AED/Utils/ProcessadorDeCandidatos.cs
+++ Trabalho AED/Utils/ProcessadorDeCandidatos.cs
@@ -4,9 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Trabalho_AED.Utils
+// Classe responsável por processar e alocar candidatos nos cursos
+public class ProcessadorDeCandidatos
 {
-    internal class ProcessadorDeCandidatos
+    public static void ProcessarCandidatos(List<Candidato> candidatos, Dictionary<int, Curso> cursos)
     {
+        foreach (var c in candidatos)
+        {
+            c.CalcularNotaMedia();
+
+            // Verifica se há vaga na 1ª opção
+            var curso1 = cursos[c.CodCurso1];
+            var curso2 = cursos[c.CodCurso2];
+
+            if (curso1.TemVaga())
+            {
+                curso1.AdicionarSelecionado(c);
+                // Libera a 2ª opção, não entra em fila de espera
+            }
+            else if (curso2.TemVaga())
+            {
+                curso2.AdicionarSelecionado(c);
+                curso1.AdicionarFilaEspera(c); // entra na fila da 1ª opção
+            }
+            else
+            {
+                // Não passou em nenhuma, entra na fila das duas opções
+                curso1.AdicionarFilaEspera(c);
+                curso2.AdicionarFilaEspera(c);
+            }
+        }
     }
 }
+
